2363feb6e2e70e8bdf459af43d555a0622f1686d
LatifahBenta <latifahmukanga@gmail.com>
2024-10-29 22:00:00 +0300

    add script

---
+++ basharrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#bioinfo=("Elminah","Chilanga","Latifah","Chacha","Cedric","Dady","Jimmy","Ibrahim")
+
+echo "What is your name?"
+
+#read name
+
+#echo Your name is $name
+
+echo "My name is" $0 $1 $2
